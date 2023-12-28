82d2ad7d5e0f1ec2fb3c923eab71932f89cf03e5
StevenTussel <149207253+StevenTussel@users.noreply.github.com>
2023-12-27 20:05:34 -0500

    Up to chapter 18
    title, still errors on employee repo, and interface ect.

--- CompanyEmployees/Service/EmployeeService.cs
+++ CompanyEmployees/Service/EmployeeService.cs
@@ -45,11 +45,12 @@ namespace Service
 
         public async Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
+            if (!employeeParameters.ValidAgeRange)
+                throw new MaxAgeRangeBadRequestException();
             await CheckIfCompanyExists(companyId, trackChanges);
             var employeesWithMetaData = await _repository.Employee
             .GetEmployeesAsync(companyId, employeeParameters, trackChanges);
-            var employeesDto =
-            _mapper.Map<IEnumerable<EmployeeDto>>(employeesWithMetaData);
+            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesWithMetaData);
             return (employees: employeesDto, metaData: employeesWithMetaData.MetaData);
         }
 
